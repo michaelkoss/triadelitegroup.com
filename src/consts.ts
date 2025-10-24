6cd02251b06242ae78855b8f9489aa3101fb245a
Michael Koss <michael@leveragesoftware.group>
2025-10-24 12:41:49 -0500

    add consts.ts and incorporate into layout

---
+++ src/consts.ts
@@ -0,0 +1,32 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Triad Elite Group';
+export const SITE_DESCRIPTION = 'Elite solutions for your business needs';
+
+// OpenGraph and Business Information
+export const COMPANY_NAME = 'Triad Elite Group';
+export const COMPANY_CITY = 'Your City';
+export const COMPANY_STATE = 'Your State';
+export const COMPANY_COUNTRY = 'USA';
+export const DEFAULT_LOCALE = 'en_US';
+export const DEFAULT_LOGO_URL = 'https://triadelitegroup.com/img/triad-logo-black.png';
+export const DEFAULT_IMAGE_WIDTH = '320';
+export const DEFAULT_IMAGE_HEIGHT = '320';
+export const PRIMARY_EMAIL = 'info@triadelitegroup.com';
+export const WEBSITE_URL = 'https://triadelitegroup.com';
+export const LINKEDIN_URL = '';
+export const REDDIT_URL = '';
+export const TIMEZONE = 'America/Chicago';
+
+// Empty values for optional fields not provided
+export const FACEBOOK_APP_ID = '';
+export const DEFAULT_AUTHOR = '';
+export const BUSINESS_TYPE = '';
+export const DEFAULT_VIDEO_URL = '';
+export const PRIMARY_PHONE = '';
+export const STREET_ADDRESS = '';
+export const TWITTER_HANDLE = '';
+
+// Analytics Configuration
+export const MICROSOFT_CLARITY_PROJECT_ID = '';
